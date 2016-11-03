79f5956f73e678d0f7a5e4c55d42842952616198
Grace Lee <eunheh@gmail.com>
2016-11-03 16:40:16 -0400

    initial commit

---
+++ src/js/class.js
@@ -0,0 +1,34 @@
+let items = new GildedRose {
+  constructor (sellIn, quality, name) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+
+let Sulfuras = new GildedRose {
+  constructor (sellIn, quality, name) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+
+let Backstage = new GildedRose {
+  constructor (sellIn, quality, name) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+
+let Conjured = new GildedRose {
+  constructor (sellIn, quality, name) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+
+let AgedBrie = new GildedRose {
+  constructor (sellIn, quality, name) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
