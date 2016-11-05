afb61b40490003384cb00b203277ca18cd460f42
Grace Lee <eunheh@gmail.com>
2016-11-04 21:39:01 -0400

    tests pass made syntax changes

--- src/js/hard.js
+++ src/js/hard.js
@@ -72,8 +72,8 @@ class GildedRose {
     if (this.quality === 0) {
       this.quality = this.quality + 1;
     }
-    this.sellIn = this.sellIn - 1;
-    this.quality = this.quality - 1;
+    this.sellIn--;
+    this.quality--;
     if (this.sellIn < 0) {
        this.quality = this.quality - 1;
     }
