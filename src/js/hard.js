bcaa7960bf1ac0985dc696025f041e13e0127b17
Grace Lee <eunheh@gmail.com>
2016-11-04 21:29:56 -0400

    all the test pass

--- src/js/hard.js
+++ src/js/hard.js
@@ -6,50 +6,80 @@ class GildedRose {
   }
 
   tick () {
-    if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (this.quality > 0) {
-        if (this.name != 'Sulfuras, Hand of Ragnaros') {
-          this.quality = this.quality - 1;
-        }
-      }
-    } else {
-      if (this.quality < 50) {
-        this.quality = this.quality + 1;
-        if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.sellIn < 11) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1;
-            }
-          }
-          if (this.sellIn < 6) {
-            if (this.quality < 50) {
-              this.quality = this.quality + 1;
-            }
-          }
-        }
-      }
-    }
-    if (this.name != 'Sulfuras, Hand of Ragnaros') {
-      this.sellIn = this.sellIn - 1;
-    }
-    if (this.sellIn < 0) {
-      if (this.name != 'Aged Brie') {
-        if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (this.quality > 0) {
-            if (this.name != 'Sulfuras, Hand of Ragnaros') {
-              this.quality = this.quality - 1;
-            }
-          }
-        } else {
-          this.quality = this.quality - this.quality;
-        }
-      } else {
-        if (this.quality < 50) {
-          this.quality = this.quality + 1;
-        }
-      }
+    if (this.name == 'Aged Brie') {
+      this.agedBrie()
+    } else if (this.name == 'Sulfuras, Hand of Ragnaros') {
+      this.sulfuras()
+    } else if (this.name == 'Conjured Mana Cake') {
+      this.conjured()
+    } else if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      this.backStage()
+    } else{
+      this.normal()
     }
   }
-}
+
+  agedBrie() {
+    this.sellIn--;
+    if (this.sellIn >= 0) {
+      this.quality = this.quality + 1;
+    } else if (this.sellIn < 0){
+      this.quality = this.quality + 2;
+    }
+    if (this.quality > 50) {
+      this.quality = 50;
+    }
+  }
+
+  sulfuras() {
+    if (this.sellIn >= 0) {
+      this.quality = 80;
+    } else if (this.sellIn < 0) {
+      this.quality = 80;
+    }
+  }
+
+  conjured() {
+    this.sellIn--;
+    if (this.sellIn >= 0) {
+      this.quality = this.quality - 2;
+    } else if (this.sellIn < 0){
+      this.quality = this.quality - 4;
+    }
+    if (this.quality < 0) {
+      this.quality = 0;
+    }
+  }
+
+  backStage () {
+    this.sellIn--;
+    if (this.sellIn >= 10) {
+      this.quality = this.quality + 1;
+      this.sellIn = this.sellIn;
+    } else if (this.sellIn >= 5){
+      this.quality = this.quality + 2;
+    } else if (this.sellIn >= 0){
+      this.quality = this.quality + 3;
+    } else {
+      this.quality = 0;
+    }
+    if (this.quality > 50) {
+      this.quality = 50;
+    }
+  }
+
+  normal () {
+    if (this.quality === 0) {
+      this.quality = this.quality + 1;
+    }
+    this.sellIn = this.sellIn - 1;
+    this.quality = this.quality - 1;
+    if (this.sellIn < 0) {
+       this.quality = this.quality - 1;
+    }
+  }
+
+};
+
 
 export { GildedRose };
